5cfc1bc3fb82c4137af6675fef913e4699d67aba
Kuldeep Sharma <kuldeepsharma78607860@gmail.com>
2026-05-21 20:40:33 +0530

    docker plugin practical

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM eclipse-temurin:17
+
+WORKDIR /app
+
+COPY target/maven-demo-1.0-SNAPSHOT.jar app.jar
+
+CMD ["java", "-jar", "app.jar"]
---
+++ src/main/java/com/kuldeep/App.java
@@ -0,0 +1,13 @@
+package com.kuldeep;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
